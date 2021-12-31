b3198f7ff3afc56182a28c5bd478bed2a012abaa
Steve Meckstroth <meckstss@gmail.com>
2021-12-31 11:00:05 -0500

    Added course change test case

--- Day02/PilotSubmarine.py
+++ Day02/PilotSubmarine.py
@@ -1,7 +1,14 @@
 
 
 class PilotSubmarine:
+    horizontal = 0
+    depth = 0
     
     def __init__(self, input: str):
-        self.depths = [tuple(d.split) for d in input]
+        self.depths = [tuple(d.split()) for d in input]
         print(self.depths)
+
+    def process_course_change(direction: str, unit: int):
+        horizontal += unit if direction == "forward" else 0
+        depth += unit if direction == "down" else 0
+        depth -= unit if direction == "up" else 0
--- tests/test_Day02.py
+++ tests/test_Day02.py
@@ -13,4 +13,14 @@ forward 2"""
     def setUp(self):
         self.ps = PilotSubmarine(self.input)
 
+    def test_process_course_change(self):
+        self.ps.process_course_change("forward", 5)
+        self.assertEqual(self.ps.horizontal, 5, "Incorrect forward change")
+
+        self.ps.process_course_change("down", 5)
+        self.assertEqual(self.ps.depth, 5, "Incorrect positive depth change")
+
+        self.ps.process_course_change("up", 3)
+        self.assertEqual(self.ps.depth, 2, "Incorrect negative depth change")
+
     
