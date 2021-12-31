a31b6955ca4680334287b8d32f5488142d24e45b
Steve Meckstroth <meckstss@gmail.com>
2021-12-31 11:25:38 -0500

    Part1

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -1,22 +1,17 @@
 import os
-from Day01.SonarDepthIncrease import SonarDepthIncrease
+from Day02.PilotSubmarine import PilotSubmarine
 
 
 class Day02:
     def __init__(self):
         with open(f"{os.path.dirname(__file__)}/input.txt") as F:
-            depths = F.readlines()
+            courses = F.read()
 
-        sdi = SonarDepthIncrease(depths)
+        ps = PilotSubmarine(courses)
 
         # Part 1
-        sdi.count_depth_increases()
-        print(f"Part1: Total number of times depth increased: {sdi.increased_count}")
-
-        # Part 2
-        sdi.rolling_sum_depth_increases(window_size=3)
-        sdi.count_depth_increases(sdi.rolling_sums)
-        print(f"Part2: Total number of times rolling sum increased: {sdi.increased_count}")
+        ps.process_all_course_changes()
+        print(f"Part1: Final horizontal: {ps.horizontal} depth: {ps.depth} result: {ps.horizontal * ps.depth}")
 
 
 if __name__ == "__main__":
