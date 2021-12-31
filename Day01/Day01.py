def548b3007de68309ab37615c3c3a3600292a68
Steve Meckstroth <meckstss@gmail.com>
2021-12-31 09:02:18 -0500

    Create Day01.py

---
+++ Day01/Day01.py
@@ -0,0 +1,23 @@
+import os
+from Day01.SonarDepthIncrease import SonarDepthIncrease
+
+
+class Day01:
+    def __init__(self):
+        with open(f"{os.path.dirname(__file__)}/input.txt") as F:
+            depths = F.readlines()
+
+        sdi = SonarDepthIncrease(depths)
+
+        # Part 1
+        sdi.count_depth_increases()
+        print(f"Part1: Total number of times depth increased: {sdi.increased_count}")
+
+        # Part 2
+        sdi.rolling_sum_depth_increases(window_size=3)
+        sdi.count_depth_increases(sdi.rolling_sums)
+        print(f"Part2: Total number of times rolling sum increased: {sdi.increased_count}")
+
+
+if __name__ == "__main__":
+    day01 = Day01()
